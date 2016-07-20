4cf808cfc95f937215959e2c78d3ce6aa49ac370
Todd Owen <todd.owen@cba.com.au>
2016-07-20 16:29:04 +1000

    Add counters to writes

--- scalding/src/main/scala/commbank/coppersmith/scalding/HiveParquetSink.scala
+++ scalding/src/main/scala/commbank/coppersmith/scalding/HiveParquetSink.scala
@@ -23,6 +23,7 @@ import au.com.cba.omnia.maestro.api._
 import commbank.coppersmith._, Feature._
 import Partitions.PathComponents
 import FeatureSink.AttemptedWriteToCommitted
+import CoppersmithStats.fromTypedPipe
 
 /**
   * Parquet FeatureSink implementation - create using HiveParquetSink.apply in companion object.
@@ -37,6 +38,7 @@ case class HiveParquetSink[T <: ThriftStruct : Manifest : FeatureValueEnc, P : T
         Execution.from(Left(AttemptedWriteToCommitted(partitionPath)))
       } else {
         val eavts = features.map(implicitly[FeatureValueEnc[T]].encode)
+          .withCounter("parquet.written")
         table.writeExecution(eavts).map(_ => Right(Set(partitionPath)))
       }
     )
